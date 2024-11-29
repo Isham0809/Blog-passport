1cb432727e1ea5f7da144ea5873553909df3f3f7
Isham0809 <isha2001blm@gmail.com>
2024-11-29 23:30:59 +0530

    first commit

---
+++ models/userSchema.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username : String,
+    email : String,
+    password : String,
+    phone: Number
+},{
+    timestamps: true
+})
+const user = mongoose.model("userModel", userSchema)
+
+module.exports = user
